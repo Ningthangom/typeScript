26ab5ca90c4d7f8058318afbcff4445a4e959f01
Ningthangom <ningthangom@gmail.com>
2022-02-23 19:05:55 +1030

    added files

--- TypeScriptComplier/app.js
+++ TypeScriptComplier/app.js
@@ -1,9 +1,8 @@
-"use strict";
 console.log("what happened here");
 // do "tsc init"
 // go to tsconfig.json 
 // add "exclude" and "include" and "files"
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
     alert("button has been click");
 });
